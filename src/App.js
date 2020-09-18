2fb2424801eae005ad7029fae30a6bc2b6cc867c
ephie oyedoh <ephieo@rocketmail.com>
2020-09-18 06:57:10 +0100

    fixing som css mistakes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
 import React from "react";
 //import testApi from "./utils/api.js";
-import Card from "./card.js"
-import InputForm from "./form.js"
+import Card from "./card.js";
+import InputForm from "./form.js";
 // import randomise from "./utils/randomise.js"
 import "./App.css";
-
-
-
 
 function App() {
   //const [output, setOutput] = React.useState("");
@@ -16,43 +13,56 @@ function App() {
   const [score2, setScore2] = React.useState(0);
   const [winner, setWinner] = React.useState(null);
 
-
-
-
   if (!input && !input2) {
-    return <InputForm setInput={setInput} setInput2={setInput2} className="inputform" />;
+    return (
+      <InputForm
+        setInput={setInput}
+        setInput2={setInput2}
+        className="inputform"
+      />
+    );
   }
   function compare(score, score2) {
-
     if (score > score2) {
-      setWinner("Player 1")
-
+      setWinner("Player 1");
     } else {
-      setWinner("Player 2")
+      setWinner("Player 2");
     }
-
   }
-
 
   return (
     <div className="App">
       <div className="btnCont">
         <div className="hero-container">
-          {(!winner) ?
+          {!winner ? (
             <>
               <div className="cards">
-                <Card heroName={input} headingName={"PLAYER 1"} setScore={setScore} className="hero1" />
-                <Card heroName={input2} headingName={"PLAYER 2"} setScore={setScore2} className="hero2" />
+                <Card
+                  heroName={input}
+                  headingName={"PLAYER 1"}
+                  setScore={setScore}
+                  className="hero1"
+                />
+                <Card
+                  heroName={input2}
+                  headingName={"PLAYER 2"}
+                  setScore={setScore2}
+                  className="hero2"
+                />
               </div>
-              <button className="fightBtn" onClick={() => {
-                compare(score, score2);
-              }
-              }
-              > Fight</button>
+              <button
+                className="fightBtn"
+                onClick={() => {
+                  compare(score, score2);
+                }}
+              >
+                {" "}
+                Fight
+              </button>
             </>
-            :
-            <div><h1>{winner} win!</h1>
-              {/* <p>{`Player 1 : ${score} points - Player 2 : ${score2} points`}</p> */}
+          ) : (
+            <div className="resultsCard">
+              <h1>{winner} win!</h1>
               <p>Player 1 : {score} points</p>
               <p>Player 2 : {score2} points</p>
 
@@ -65,11 +75,12 @@ function App() {
                   setScore2(0);
                   setWinner(null);
                 }}
-
-              >RESET</button>
-            </div>}
+              >
+                RESET
+              </button>
+            </div>
+          )}
         </div>
-
 
         {/* <button className="rdmFightBtn" onClick={randomise}>Random Fight</button> */}
       </div>
--- src/card.js
+++ src/card.js
@@ -4,40 +4,38 @@ import testApi from "./utils/api.js";
 // import svg from "/public/icons8-captain-america.svg"
 
 function Card({ heroName, headingName, setScore }) {
-    // const [text, setText] = React.useState("");
-    const [data, setData] = React.useState(null);
+  // const [text, setText] = React.useState("");
+  const [data, setData] = React.useState(null);
 
-    React.useEffect(() => {
-        const hero = heroName;
-        testApi(hero).then((data) => {
-            setData(data)
-            setScore(data.data.results[0].comics.available)
-        });
-    }, [heroName, setScore]);
-    if (!data) return (
-        <div>
-            <img src="logo.svg" alt="loadingImg" className="avenger" />
-            <h1>....Loading {heroName} </h1>
-        </div>)
-    const result = data.data.results[0];
-    const img = data.data.results[0].thumbnail.path + ".jpg";
-    // const score = result.comics.available;
-
-
-
+  React.useEffect(() => {
+    const hero = heroName;
+    testApi(hero).then((data) => {
+      setData(data);
+      setScore(data.data.results[0].comics.available);
+    });
+  }, [heroName, setScore]);
+  if (!data)
     return (
-        <div className="App">
-            <header className="App-header">
-                <div className="hero">
-                    <h1>{headingName}</h1>
-                    <img src={img} alt="" id="img-alter"></img>
-                    <h2>{result.name}</h2>
-                    {/* <h2>Score:{result.comics.available}</h2> */}
-
-                </div>
-            </header>
-        </div>
+      <div>
+        <h1>....Loading {heroName} </h1>
+      </div>
     );
+  const result = data.data.results[0];
+  const img = data.data.results[0].thumbnail.path + ".jpg";
+  // const score = result.comics.available;
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="hero">
+          <h1>{headingName}</h1>
+          <img src={img} alt="" id="img-alter"></img>
+          <h2>{result.name}</h2>
+          {/* <h2>Score:{result.comics.available}</h2> */}
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default Card;
